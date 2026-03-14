bf0ea92abab9cc6c2ef21f6364d218edddfb8205
Mayank Malhotra <mayankmalhotra772@gmail.com>
2026-03-14 22:28:14 +0530

    strengthen DB password for deployment security

--- docker/create_accounts.php
+++ docker/create_accounts.php
@@ -11,7 +11,7 @@ $host = getenv('DB_HOST') ?: 'db';
 $port = getenv('DB_PORT') ?: '5432';
 $name = getenv('DB_NAME') ?: 'transactiwar';
 $user = getenv('DB_USER') ?: 'twuser';
-$pass = getenv('DB_PASS') ?: 'tw_s3cur3_p@ss!';
+$pass = getenv('DB_PASS') ?: 'G4uYTFdF!YAunKjO&mJ5nB9sW1YGFNqI';
 
 $dsn = "pgsql:host={$host};port={$port};dbname={$name}";
 
